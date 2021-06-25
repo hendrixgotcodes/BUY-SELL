dc58677eccd595c357ce07efcd3de822b3af7fa6
Samuel Opoku Asare <asare11samuel@gmail.com>
2021-06-25 12:17:21 +0000

    Form validation with Yup

--- app/components/Screens/LoginScreen.jsx
+++ app/components/Screens/LoginScreen.jsx
@@ -1,60 +1,72 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {View, StyleSheet, Image, TextInput} from 'react-native'
 
 //Components
 import {Formik} from 'formik'
 import SafeAreaScreen from './SafeAreaScreen'
 import AppTextInput from '../AppTextInput'
+import AppText from '../AppText'
 import AppButton from '../AppButton'
 
 //Assets
 const logo = require("../../assets/logo-red.png")
 
+//Extra
+import * as Yup from 'yup'
+
+//Variable
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("Email"),
+    password: Yup.string().required().min(5).label("Password")
+})
+
 export default function LoginScreen(){
 
-    const [email, setEmail] = useState() 
-    const [password, setPassword] = useState() 
-
     return(
-        <SafeAreaScreen style={styles.container}>
+        <SafeAreaScreen>
         
-            <Image source={logo} style={styles.logo} />
+            <View style={styles.container}>
+                <Image source={logo} style={styles.logo} />
             
-            <Formik
-                initialValues={{email: "", password: ""}}
-                onSubmit={(values)=>console.log(values)}
-            >
+                <Formik
+                    initialValues={{email: "", password: ""}}
+                    onSubmit={(values)=>console.log(values)}
+                    validationSchema={validationSchema}
+                >
 
-                {
-                    ({handleSubmit, handleChange})=>(
-                            <>
-                                <AppTextInput 
-                                    autoCapitalize="none"
-                                    autoCorrect={false}
-                                    icon="email"
-                                    keyboardType="email-address"
-                                    onChangeText = {handleChange("email")}
-                                    placeholder="Email"
-                                    textContentType="emailAddress"
-                                />
-                                <AppTextInput 
-                                    autoCapitalize="none"
-                                    autoCorrect={false}
-                                    icon="lock"
-                                    onChangeText = {handleChange("password")}
-                                    placeholder="Password"
-                                    secureTextEntry={true}
-                                    textContentType="password"
-                                />
-                                <AppButton 
-                                    title="Login"
-                                    onPress={handleSubmit}
-                                />
-                            </>
-                    )
-                }
+                    {
+                        ({handleSubmit, handleChange, errors})=>(
+                                <>
+                                    <AppTextInput 
+                                        autoCapitalize="none"
+                                        autoCorrect={false}
+                                        icon="email"
+                                        keyboardType="email-address"
+                                        onChangeText = {handleChange("email")}
+                                        placeholder="Email"
+                                        textContentType="emailAddress"
+                                    />
+                                    <AppText style={{color: "red"}}>{errors.email}</AppText>
+                                    <AppTextInput 
+                                        autoCapitalize="none"
+                                        autoCorrect={false}
+                                        icon="lock"
+                                        onChangeText = {handleChange("password")}
+                                        placeholder="Password"
+                                        secureTextEntry={true}
+                                        textContentType="password"
+                                    />
+                                    <AppText style={{color: "red"}}>{errors.password}</AppText>
+                                    <AppButton 
+                                        title="Login"
+                                        onPress={handleSubmit}
+                                    />
+                                </>
+                        )
+                    }
 
-            </Formik>
+                </Formik>
+            </View>
 
         </SafeAreaScreen>
     )
