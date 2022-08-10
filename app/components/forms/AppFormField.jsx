6667173dd8eebb544e28a64cdbaf3f0c3a4f3126
Samuel O. Asare <asare11samuel@gmail.com>
2022-08-10 01:01:07 +0000

    fix: fixed weird padding on FavoritesScreen

--- app/components/forms/AppFormField.jsx
+++ app/components/forms/AppFormField.jsx
@@ -1,1 +1,33 @@
-import { useFormikContext } from "formik";import React from "react";import { View, StyleSheet } from "react-native";// Componentsimport Colors from "../../assets/_colors";import AppTextInput from "../AppTextInput";import ErrorMessage from "./ErrorMessage";// Assets// Hookexport default function AppFormField({ name, style, ...rest }) {    const {        setFieldTouched,        setFieldValue,        handleChange,        errors,        touched,        values,    } = useFormikContext();    return (        <>            <AppTextInput                // onChangeText = {handleChange(name)}                onChangeText={(text) => setFieldValue(name, text)}                value={values[name]}                onBlur={() => setFieldTouched(name)}                style={style}                {...rest}            />            {touched[name] && <ErrorMessage message={errors[name]} />}        </>    );}
+import { useFormikContext } from "formik";
+import React from "react";
+
+// Components
+import AppTextInput from "../AppTextInput";
+import ErrorMessage from "./ErrorMessage";
+
+// Assets
+
+// Hook
+
+export default function AppFormField({ name, style, ...rest }) {
+    const {
+        setFieldTouched,
+        setFieldValue,
+        errors,
+        touched,
+        values,
+    } = useFormikContext();
+
+    return (
+        <>
+            <AppTextInput
+                onChangeText={(text) => setFieldValue(name, text)}
+                value={values[name]}
+                onBlur={() => setFieldTouched(name)}
+                style={style}
+                {...rest}
+            />
+            {touched[name] && <ErrorMessage message={errors[name]} />}
+        </>
+    );
+}
